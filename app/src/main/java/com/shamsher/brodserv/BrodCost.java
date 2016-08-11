48edae3cd8ba956ac5df98f7c362389428fb1ec1
princekumar14763 <princekumar14763@gmail.com>
2016-08-11 20:29:58 +0530

    change in main java file

--- app/src/main/java/com/shamsher/brodserv/BrodCost.java
+++ app/src/main/java/com/shamsher/brodserv/BrodCost.java
@@ -1,7 +1,9 @@
+//This is my git project.
 package com.shamsher.brodserv;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
 import android.content.Intent;
 
 /**
@@ -11,5 +13,6 @@ public class BrodCost extends BroadcastReceiver{
     @Override
     public void onReceive(Context context, Intent intent) {
         context.startService(new Intent(context,ServiceS.class));
+		Toast.makeText(context,"sdfnygnfyda",Toast.LENGTH_LONG).show();
     }
 }
