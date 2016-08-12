c0165040d5dea58d3b8cd4f720be406baa22cd27
shamsher400 <shamsher.singh400@gmail.com>
2016-08-12 16:01:28 +0530

    //About home layout add

--- app/src/main/java/com/shamsher/brodserv/BrodCost.java
+++ app/src/main/java/com/shamsher/brodserv/BrodCost.java
@@ -4,7 +4,7 @@ package com.shamsher.brodserv;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.Intent;
+import android.widget.Toast;
 
 /**
  * Created by shamsher on 22/6/16.
@@ -13,6 +13,6 @@ public class BrodCost extends BroadcastReceiver{
     @Override
     public void onReceive(Context context, Intent intent) {
         context.startService(new Intent(context,ServiceS.class));
-		Toast.makeText(context,"sdfnygnfyda",Toast.LENGTH_LONG).show();
+		Toast.makeText(context,"sdfny gnf yda",Toast.LENGTH_LONG).show();
     }
 }
